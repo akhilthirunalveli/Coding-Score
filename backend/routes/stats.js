74c6caf9748fb25e4d44c8f1b98c9f66d6914838
Akhil Thirunalveli <thirunalveliakhil@gmail.com>
2024-12-09 12:52:48 +0530

    Delete backend directory

--- backend/routes/stats.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { fetchUserStats } = require('./controllers/statsController');
-const router = express.Router();
-
-router.get('/:platform/:handle', fetchUserStats);
-
-module.exports = router;
